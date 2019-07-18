ce24e326057febf1f85aac36cdd66db93ad51213
Gary Storey <gary@digitalcrafts.com>
2019-07-17 21:31:38 -0400

    Thursday

---
+++ 4 - Thu/boxes/index.js
@@ -0,0 +1,14 @@
+console.log('*********');
+const boxContainer = document.querySelector('.box-container');
+const boxes = document.querySelectorAll('.box');
+/**********************************
+ * Add a form for the user to input an x and a y coordinate
+ * and when the user clicks an "Add Box", it will add a new
+ * "box" element to the page at that x/y coordinate INSIDE
+ * THE BOX CONTAINER.
+ * 
+ * The HTML file is linked to both the CSS file and 
+ * this JS file.
+ * 
+ * Have fun!
+ ***********************************/
